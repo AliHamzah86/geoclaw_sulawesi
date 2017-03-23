df3aa44d33817092bf24c6f939ec8b9ea7706d09
Randy LeVeque <rjl@uw.edu>
2017-03-22 22:50:24 -0700

    fix path for downloading npy files

--- rsync_all_runs.sh
+++ rsync_all_runs.sh
@@ -11,10 +11,12 @@
 # After running this, move geoclaw_output_via_rsync/all_runs_npy_files
 # to geoclaw_output/all_runs_npy_files  for further analysis scripts to work
 
+# Note the total file size is about 787M.
+
 mkdir -p geoclaw_output_via_rsync
 
 rsync -avz \
-    ptha@homer.u.washington.edu:public_html/fema2016/all_runs_npy_files \
+    ptha@homer.u.washington.edu:public_html/fema_source_filter/all_runs_npy_files \
     geoclaw_output_via_rsync/
 
 
