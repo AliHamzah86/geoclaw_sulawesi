6b984e8025c72647c8563c9045510e4b75abec79
Randy LeVeque <rjl@uw.edu>
2018-07-30 07:18:21 -0700

    added etopo1_-126_-123_40_45_1min.asc to get_topo.sh

--- DataFiles/get_topo.sh
+++ DataFiles/get_topo.sh
@@ -1,3 +1,4 @@
 
 wget http://depts.washington.edu/ptha/FEMA/etopo1_-130_-124_38_45_1min.asc
+wget http://depts.washington.edu/ptha/FEMA/etopo1_-126_-123_40_45_1min.asc 
 wget http://depts.washington.edu/ptha/FEMA/cc-1sec.asc
